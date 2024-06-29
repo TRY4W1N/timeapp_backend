50239066bafba3a0df8c4d164f7822e2a89b577d
JaysesS <myfantasyisnotworking@vk.com>
2024-06-29 15:14:35 +0300

    Add test user auth for endpoints

--- src/infrastructure/di/providers/service_application.py
+++ src/infrastructure/di/providers/service_application.py
@@ -1,6 +1,7 @@
 from dishka import Provider, Scope, provide
 
 from src.application.service.auth.firebase.service import AuthServiceFirebase
+from src.domain.ctx.auth.dto import UserIdentityDTO
 from src.domain.ctx.auth.interface.service import AuthService
 from src.infrastructure.di.alias import ConfigType
 
@@ -12,4 +13,17 @@ class AppServiceProvider(Provider):
     async def get_auth_service(self, config: ConfigType) -> AuthService:
         service = AuthServiceFirebase(name=config.APP_NAME, secret_path=config.FIREBASE_SECRET_PATH)
         service.setup()
+
+        if config.APP_ENV == "DEV":
+            async def get_by_token_mocked(token: str) -> UserIdentityDTO:
+                if token not in config.DEV_USERS:
+                    raise ValueError("Invalid token")
+                user_dev_data = config.DEV_USERS[token]
+                return UserIdentityDTO(
+                    id=user_dev_data["id"],
+                    name=user_dev_data["name"],
+                    email=user_dev_data["email"],
+                )
+            service.get_by_token = get_by_token_mocked
+
         return service
